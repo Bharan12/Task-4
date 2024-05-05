02b55f21f211feb985a00eaa0d5aa1f55826e408
Bharan12 <bharanidharan1231@gmail.com>
2024-05-05 23:34:07 +0530

    New file added

---
+++ index.js
@@ -0,0 +1,47 @@
+let myResume={
+    "basics": {
+        "name": "Bharanidharan T",
+        "email": "Bharanidharan1231@gamil.com",
+        "phone": 8072241368,
+        "degree": "B.com",
+        "location": {
+        "address": "7/15 Brindhavan colony Coimbatore",
+        "postalCode": 641015,
+        "city": "Coimbatore",
+        "state": "Tamilnadu",
+      },
+    },
+    "work": [
+      {
+        "company": "Hindusthan college of arts and science",
+        "position": "HR Admin",
+        "startDate": "01/06/2023",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Sri Krishna college of arts and science",
+        "department": "Commerce",
+        "batch start year": 2018,
+        "batch end year": 2021,
+        "Percentage": "60%",
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,youtuber,",
+      }
+    ]
+  }
+  console.log(myResume);
