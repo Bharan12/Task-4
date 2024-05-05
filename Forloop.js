4f5c1a71d3b3eac9ea4d6fd820d503ed981189ee
Bharan12 <bharanidharan1231@gmail.com>
2024-05-06 00:27:19 +0530

    added new files

---
+++ Forloop.js
@@ -0,0 +1,29 @@
+let json = [
+    {
+    Name : "Bharanidharan", 
+    mail : "Bharani@gmail.com",
+    Course : "Full stack development",
+    Task  : "For the given JSON iterate over all for loops (for, for in, for of, forEach)"
+  }
+]
+
+//For loop
+  for(let i = 0; i < json.length; i++) {
+  console.log(json);
+  }
+
+
+//For in
+for (let i in json) {
+  console.log(json[i])
+}
+
+//For of
+for (let i of json){
+    console.log(json)
+}
+
+//For each
+json.forEach((e)=>{
+    console.log(e)
+})
